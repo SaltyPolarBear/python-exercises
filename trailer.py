f785f51c2605607f44498d65ddc28aba0f0b665e
SaltyPolarBear <39627631+SaltyPolarBear@users.noreply.github.com>
2018-05-25 10:34:15 -0500

    Create trailer.py

---
+++ trailer.py
@@ -0,0 +1,50 @@
+import time
+from time import sleep
+print ("The", end=' ', flush=True)
+sleep(0.3)
+print ("Legend", end=' ', flush=True)
+sleep(2)
+print ("of", end=' ', flush=True)
+sleep(0.1)
+print ("Rogue!\n")
+sleep(0.5)
+print ('heeeeeeey coool guy, ply muh gamye!')
+sleep(1.5)
+print ('I garuntey you will have good tim')
+sleep(1.5)
+print ("cool game mechanics:\n")
+sleep(1.5)
+print("hte gbline used smacck!!! what ugonna due?:")
+sleep(1.5)
+input()
+print('\x1b[1;31m'+'Traceback (most recent call last)\n'+'  File "<pyshell#2>, line 1, in <module>\n'+'TypeError: unsupported operand type(s) for +: for "int" and "str"'+'\x1b[0m')
+sleep(2)
+print ("oh... soury... well be fexed sooon.")
+sleep(1.5)
+print ("we hve goood stry!!!!\n")
+sleep(1)
+print ("Squid on top of the queens head: hoyhoiyoyhoyhoyhoy yellow boois, why not go to the pit of lava it feels real nice garunteed.\n")
+sleep(5)
+print ("every person in the kingdom: oh ok\n")
+sleep(3)
+print ("Our main protaganist, Rogue: wait a minute...\n")
+sleep(10)
+print ("okeee well thas ouur ga,me ")
+sleep(1.5)
+print ("we doont kno whhen it willl bey donee..")
+print ("gobye")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
