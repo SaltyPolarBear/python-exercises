76e34b382089f90dc50040c9fc776f75e5ed4f9e
SaltyPolarBear <39627631+SaltyPolarBear@users.noreply.github.com>
2018-05-25 10:45:56 -0500

    Create A9.py

---
+++ A9.py
@@ -0,0 +1,8 @@
+# Take user input
+wrd=input("Please enter a word:")
+rvs=wrd[::-1]
+#print(rvs)
+if wrd == rvs:
+    print("This word is a palindrome")
+else:
+  print("This word is not a palindrome")
